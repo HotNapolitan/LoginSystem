6c98d292db982a5c3ae6c8986e604e9893a47e40
HotNapolitan <goldsin01@gmail.com>
2022-01-31 00:36:06 +0900

    Changed the login authnetication method

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,15 +9,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.example.demo.auth.DatabaseUserDetailsService;
+
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DatabaseUserDetailsService userDetailsService;
 
-	//Userの認証方式を決定するメソッド
+//	//Userの認証方式を決定するメソッド
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		//user情報をメモリ上で管理していること
+//		auth.inMemoryAuthentication()
+//		.withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+//	}
+	
+	//DatabaseUserDetailsServiceのインスタンスを設定する
+	//ログインの機能をmysql＆mybatisを使っているようになる
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//user情報をメモリ上で管理していること
-		auth.inMemoryAuthentication()
-		.withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+		auth.userDetailsService(userDetailsService);
 	}
 
 	//WEBアプリケーションが管理しているリソースへのアクセス制御の設定をするメソッド
